69bb08531d166f2cbf169373be14b88a55ec3107
LittleHaku <ivandelhorno@gmail.com>
2023-05-02 20:11:02 +0200

    nueva vision

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,54 +1,55 @@
 <template>
   <div>
-    <div class="container my-5">
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <h1 class="text-center mb-4">Join Kahoot Game</h1>
-          <form @submit.prevent="joinGame">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="alias">Alias:</label>
-                  <input type="text" class="form-control" id="alias" v-model="alias" required>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label for="gameId">Game ID:</label>
-                  <input type="number" class="form-control" id="gameId" v-model="gameId" required>
-                </div>
-              </div>
-            </div>
-            <button type="submit" class="btn btn-primary btn-block mt-4">Join Game</button>
-          </form>
-          <p v-if="error" class="text-danger mt-3">{{ error }}</p>
+    <div class="container mt-3">
+      <h1>Join Kahoot Game</h1>
+      <form @submit.prevent="joinGame">
+        <div class="form-group">
+          <label for="alias">Alias:</label>
+          <input type="text" class="form-control" id="alias" v-model="alias" required>
         </div>
-      </div>
+        <div class="form-group">
+          <label for="gameId">Game ID:</label>
+          <input type="number" class="form-control" id="gameId" v-model="gameId" required>
+        </div>
+        <button type="submit" class="btn btn-primary btn-p">Join Game</button>
+      </form>
+      <p v-if="error" class="text-danger mt-3">{{ error }}</p>
     </div>
   </div>
 </template>
 
 <style>
 form {
+  display: flex;
+  flex-direction: column;
   background-color: #fff;
   padding: 25px;
   border-radius: 10px;
   box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
 }
 
-label {
-  font-weight: 600;
+.form-group {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
 }
 
-input[type="text"],
-input[type="number"] {
+.form-group label {
+  font-weight: 600;
+  margin-bottom: 5px;
+}
+
+.form-group input[type="text"],
+.form-group input[type="number"] {
   border: 2px solid #ddd;
   border-radius: 4px;
   box-shadow: none;
+  padding: 10px;
+  margin-bottom: 10px;
 }
 
-input[type="text"]:focus,
-input[type="number"]:focus {
+.form-group input[type="text"]:focus,
+.form-group input[type="number"]:focus {
   border: 2px solid #007bff;
   box-shadow: none;
 }
@@ -69,11 +70,12 @@ input[type="number"]:focus {
 }
 </style>
 
+
 <script>
 import axios from 'axios';
 
 export default {
-  name: 'JoinGameView',
+  name: 'HomeView',
 
   data() {
     return {
@@ -85,21 +87,25 @@ export default {
 
   methods: {
     joinGame() {
-      this.$store.commit('setAlias', this.alias);
-      this.$store.commit('setGameID', this.gameId);
-
-      const payload = {
-        game: this.gameId,
+      this.$store.commit("setAlias", this.alias);
+      this.$store.commit("setGameID", this.gameId);
+      const info = {
         alias: this.alias,
-        uuidP: this.uuidP,
+        gameId: this.gameId,
+        uuidp: this.uuidP
       };
 
-      axios.post('/api/participant/', payload, {
-        withCredentials: false,
+      axios.post('/api/participant/', {
+        game: this.gameId,
+        alias: this.alias,
+        uuidP: this.uuidP
+      }, {
+        withCredentials: false
+      }, {
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
+          'Access-Control-Allow-Origin': '*'
+        }
       }).then(response => {
         this.$store.commit('setUuidP', response.data.uuidP);
         this.error = null;
@@ -108,7 +114,9 @@ export default {
         console.log(error);
         this.error = error.response.data.error;
       });
-    },
-  },
-};
+
+      this.oldGameId = this.gameId;
+    }
+  }
+}
 </script>
